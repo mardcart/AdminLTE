5943e757615e7b7093c6dc20f425206540757fed
mardcart <mardcart.9@gmail.com>
2020-05-07 00:20:46 -0300

    se termino el proyecto

--- template/modulos/panel.php
+++ template/modulos/panel.php
@@ -1,7 +1,23 @@
 <?php  
 
 include('global/sessiones.php');
+include('global/conexion.php');
  ///echo "hola soy panel en modulos";
+
+ $sentenciaSQL = $pdo->prepare("select count(*) totalventas from tblventas where PaypalDatos<>'' ");
+ $sentenciaSQL->execute();
+ $registro=$sentenciaSQL->fetch(PDO::FETCH_ASSOC);
+ $totalVentas=$registro['totalventas'];
+
+ $sentenciaSQL = $pdo->prepare("select count(*) totalventasPendientes from tblventas where PaypalDatos='' ");
+ $sentenciaSQL->execute();
+ $registro=$sentenciaSQL->fetch(PDO::FETCH_ASSOC);
+ $totalVentasPendientes=$registro['totalventasPendientes'];
+
+ $sentenciaSQL = $pdo->prepare("select count(*) totalventas, sum(total) as ingresoTotal from tblventas where PaypalDatos='' ");
+ $sentenciaSQL->execute();
+ $registro=$sentenciaSQL->fetch(PDO::FETCH_ASSOC);
+ $ingresoTotal=$registro['ingresoTotal'];
 
 
 ?>
--- template/vistaPanel.php
+++ template/vistaPanel.php
@@ -38,14 +38,13 @@
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>150</h3>
+                <h3><?php  echo $totalVentas; ?></h3>
 
-                <p>New Orders</p>
+                <p>Ordenes concretadas</p>
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
@@ -53,14 +52,14 @@
             <!-- small box -->
             <div class="small-box bg-success">
               <div class="inner">
-                <h3>53<sup style="font-size: 20px">%</sup></h3>
+                <h3><?php echo $totalVentasPendientes; ?></h3>
 
-                <p>Bounce Rate</p>
+                <p>Ordenes Pendientes</p>
               </div>
               <div class="icon">
                 <i class="ion ion-stats-bars"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+
             </div>
           </div>
           <!-- ./col -->
@@ -68,14 +67,14 @@
             <!-- small box -->
             <div class="small-box bg-warning">
               <div class="inner">
-                <h3>44</h3>
+                <h3>$ <?php echo number_format($ingresoTotal,2);?></h3>
 
-                <p>User Registrations</p>
+                <p>total ingresos</p>
               </div>
               <div class="icon">
                 <i class="ion ion-person-add"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              
             </div>
           </div>
           <!-- ./col -->
